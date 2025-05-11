e8d37c6159b861430654d707c740d643cca6edde
Anurag-Lamba <anuraglambra.786@gmail.com>
2025-05-11 18:15:10 +0530

    openai and ollama, ollama monitoring not able to see again ssl issue.

--- Gen-AI/c. Basics+Of+Langchain/1.2-ollama/app.py
+++ Gen-AI/c. Basics+Of+Langchain/1.2-ollama/app.py
@@ -1,17 +1,26 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.llms import Ollama
+# from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
+
+import httpx
+from langsmith import client
+client._sync_client = httpx.Client(verify=False)
+
 
 load_dotenv()
 
 ## Langsmith Tracking
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_TRACING"]="true"
 os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+print(os.environ["LANGCHAIN_PROJECT"])
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
@@ -27,7 +36,7 @@ input_text=st.text_input("What question you have in mind?")
 
 
 ## Ollama Llama2 model
-llm=Ollama(model="gemma:2b")
+llm=OllamaLLM(model="gemma:2b")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
